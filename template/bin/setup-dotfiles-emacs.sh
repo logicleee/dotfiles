2d5cad18335a241a2d30a8fd5b9c777fc51b75a1
lee <logicleeee+github@gmail.com>
2020-06-25 21:28:53 -0700

    updated setup-emacs

--- template/bin/setup-dotfiles-emacs.sh
+++ template/bin/setup-dotfiles-emacs.sh
@@ -7,6 +7,33 @@ CODE_PATH="$(cat $code_pathfile)" || exit
 EMACS_CONFIG_PATH="$(cat $emacsconfig_pathfile)" || exit
 emacs_d="$EMACS_CONFIG_PATH/emacs.d"
 
+dotfiles_emacs_clone_purcells_config () {
+    cd "$EMACS_CONFIG_PATH/"
+    git clone https://github.com/purcell/emacs.d.git
+    dotfiles_emacs_link_files
+    cd -
+}
+
+dotfiles_emacs_update_config () {
+    cd "$emacs_d/"
+    git pull
+    cd -
+}
+
+dotfiles_emacs_link_files () {
+    [ -d "$emacs_d/lisp-local" ] && rm -r "$emacs_d/lisp-local"
+    _if_exists_move_but_backup_item "$emacs_d/lisp/init-local.el"
+
+    #TODO need to put emacs-local into github and clone here
+
+    _link_item "$CODE_PATH/emacs-lisp-local/init-local.el" \
+        "$emacs_d/lisp/init-local.el"
+    _link_item "$CODE_PATH/emacs-lisp-local" "$emacs_d/lisp-local"
+    _link_item "$CODE_PATH/emacs-capture-templates" "$emacs_d/lisp-local/templates"
+    _link_item "$CODE_PATH/emacs-jade-mode" "$emacs_d/lisp-local/jade-mode"
+    _link_item "$CODE_PATH/emacs-themes" "$emacs_d/themes"
+}
+
 # check if setup done flag && exit
 if [ -f "$EMACS_CONFIG_PATH/setup_emacs_complete.flag" ]
 then 
@@ -14,37 +41,11 @@ then
     exit
 fi
 
-# setup emacs
-# clone purcell's repo
-
 if [ ! -d "$EMACS_CONFIG_PATH/emacs.d" ]
 then
-
-    cd "$EMACS_CONFIG_PATH/"
-
-    git clone https://github.com/purcell/emacs.d.git
-
-    [ -d "$emacs_d/lisp-local" ] && rm -r "$emacs_d/lisp-local"
-    _if_exists_move_but_backup_item "$emacs_d/lisp/init-local.el"
-
-    ln -s "$CODE_PATH/emacs-lisp-local/init-local.el" \
-        "$emacs_d/lisp/init-local.el"
-    ln -s "$CODE_PATH/emacs-lisp-local" "$emacs_d/lisp-local"
-    ln -s "$CODE_PATH/emacs-capture-templates" "$emacs_d/lisp-local/templates"
-    ln -s "$CODE_PATH/emacs-jade-mode" "$emacs_d/lisp-local/jade-mode"
-    ln -s "$CODE_PATH/emacs-themes" "$emacs_d/themes"
-
-    cd -
-
+    dotfiles_emacs_clone_purcells_config
 else
-
-    cd "$emacs_d/"
-    git pull
-    cd -
-
+    dotfiles_emacs_update_config
 fi
 
-# clone my local-lisp
-# link
-cd -
 echo "$(date) INFO: COMPLETED ${BASH_SOURCE[0]}"
