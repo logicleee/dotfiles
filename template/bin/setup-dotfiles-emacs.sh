feb760fc30046597efc0f2a214ad9b53a3bf224f
lee <logicleeee+github@gmail.com>
2020-06-26 20:17:46 -0700

    adding emacs config

--- template/bin/setup-dotfiles-emacs.sh
+++ template/bin/setup-dotfiles-emacs.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 source "$DOTFILES_PATH/lib/dotfiles-utils.lib.sh" || exit
-code_pathfile="$HOME/.VAR_CODE_PATH"
 emacsconfig_pathfile="$HOME/.VAR_EMACS_CONFIG_PATH"
-CODE_PATH="$(cat $code_pathfile)" || exit
 EMACS_CONFIG_PATH="$(cat $emacsconfig_pathfile)" || exit
 emacs_d="$EMACS_CONFIG_PATH/emacs.d"
+site_lisp="$EMACS_CONFIG_PATH/emacs.d/site-lisp"
 
-dotfiles_emacs_clone_purcells_config () {
+dotfiles_emacs_install_purcells_config () {
     cd "$EMACS_CONFIG_PATH/"
     git clone https://github.com/purcell/emacs.d.git
     dotfiles_emacs_link_files
+    cd -
+}
+
+dotfiles_emacs_install_jade_mode () {
+    cd "$site_lisp"
+    git clone https://github.com/brianc/jade-mode.git
+    cd -
+}
+
+dotfiles_emacs_install_theme_solarized () {
+    cd "$site_lisp"
+    git clone https://github.com/sellout/emacs-color-theme-solarized.git
     cd -
 }
 
@@ -26,12 +37,12 @@ dotfiles_emacs_link_files () {
 
     #TODO need to put emacs-local into github and clone here
 
-    _link_item "$CODE_PATH/emacs-lisp-local/init-local.el" \
+    _link_item "$DOTFILES_PATH/emacs-lisp-local/init-local.el" \
         "$emacs_d/lisp/init-local.el"
-    _link_item "$CODE_PATH/emacs-lisp-local" "$emacs_d/lisp-local"
-    _link_item "$CODE_PATH/emacs-capture-templates" "$emacs_d/lisp-local/templates"
-    _link_item "$CODE_PATH/emacs-jade-mode" "$emacs_d/lisp-local/jade-mode"
-    _link_item "$CODE_PATH/emacs-themes" "$emacs_d/themes"
+    _link_item "$DOTFILES_PATH/emacs-lisp-local" "$emacs_d/lisp-local"
+    _link_item "$DOTFILES_PATH/emacs-capture-templates" "$emacs_d/lisp-local/templates"
+    _link_item "$DOTFILES_PATH/emacs-jade-mode" "$emacs_d/lisp-local/jade-mode"
+    _link_item "$DOTFILES_PATH/emacs-themes" "$emacs_d/themes"
 }
 
 # check if setup done flag && exit
@@ -41,9 +52,11 @@ then
     exit
 fi
 
-if [ ! -d "$EMACS_CONFIG_PATH/emacs.d" ]
+if [ ! -d "$emacs_d" ]
 then
-    dotfiles_emacs_clone_purcells_config
+    dotfiles_emacs_install_purcells_config
+    dotfiles_emacs_install_jade_mode
+    dotfiles_emacs_install_theme_solarized
 else
     dotfiles_emacs_update_config
 fi
---
+++ template/emacs-lisp-local/init-org-capture-templates.el
@@ -0,0 +1,33 @@
+;; update org capture templates
+;; BEGIN org-mode capture templates
+
+;;; Code:
+
+(defvar org-default-notes-file "~/org/_gtd.org")
+(setq org-capture-templates
+      `(("t" "todo" entry
+         (file+headline "~/org/_refile-term.org" "INBOX") ;
+         (file "~/org/templates/todo.org") :clock-resume t)
+        ("r" "Reading List" entry
+         (file "~/org/p-reading-list.org")
+         (file "~/org/templates/reading-list.org"))
+        ("j" "Job Application" entry
+         (file "~/Dropbox/org/r-job-app-log-2019.org")
+         (file "~/org/templates/job-app.org"))
+        ("c" "Call Log Entry" entry
+         (file+headline "~/org/_refile-term.org" "INBOX") ;
+         (file "~/org/templates/call-log.org"))
+        ("o" "Journal Entry" entry
+         (file "~/Dropbox/.org-journal/.journal.org")
+         (file "~/org/templates/journal.org"))
+  	("d" "Review: Daily Review" entry
+  	 (file+datetree "~/org/r-gtd-reviews-daily.org")
+  	 (file "~/org/templates/review-daily-template.org") :tree-type week)
+  	("w" "Review: Weekly Review" entry
+  	 (file+datetree "~/org/r-gtd-reviews-weekly.org")
+  	 (file "~/org/templates/review-weekly-template.org") :tree-type week)
+        ))
+
+;; END org-mode capture templates
+
+(provide 'init-org-capture-templates)
