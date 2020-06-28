adbe734dee686d32bd71d94546722979d5ab64a3
lee <logicleeee+github@gmail.com>
2020-06-28 11:18:04 -0700

    updated system tools

--- template/bin/software-update-playbook.sh
+++ template/bin/software-update-playbook.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 export HOMEBREW_NO_AUTO_UPDATE=1
-source lib/Darwin.install.lib.sh || exit 1
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source ~/.lib/Darwin.install.lib.sh || exit 1
+
+if ! cd ~/.system-setup
+then
+    echo "ERROR: Missing ~/.system-setup -- Exiting"
+    exit 1
+fi
 
 _sudo_keep_alive
 brew update                                                       && \
 brew upgrade                                                      && \
 brew cask upgrade                                                 && \
-ansible-playbook software-update-playbook.yml -i config/inventory && \
+ansible-playbook software-update-playbook.yml -i config/inventory \
+   --ask-become-pass && \
 sudo softwareupdate --install --all --restart
